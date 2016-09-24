f3231af4a342831b97279d4fba36bcb6e9d9b3b0
KavinSub <kavin.a.subramanyam@gmail.com>
2016-09-23 22:01:18 -0700

    Add description, remove unused fonts.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 $(".yes-button").each(function(){
 	$(this).click(emailMe);
-})
+});
+
+window.onload = setSkillContainerHeights;
+window.onresize = setSkillContainerHeights;
+$(window).on("orientationchange", setSkillContainerHeights);
+
 
 function emailMe(){
 	window.location.href = "mailto:kavin.a.subramanyam@gmail.com?subject=What\'s up\?";
 }
+
+// ffs css
+function setSkillContainerHeights(){
+	$("#largest-skill-container").css("height", "auto");
+	var largestContainerHeight = $("#largest-skill-container").css("height");
+	$(".skill-section-container").each(function(){
+		$(this).css("height", largestContainerHeight);
+	});
+}
+
