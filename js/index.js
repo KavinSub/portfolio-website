b1e11cbb0349b37d4182bad896668747b48ad6cb
KavinSub <kavin.a.subramanyam@gmail.com>
2016-09-23 17:57:56 -0700

    Add social media icons, refactor footer to make more responsive, other minor changes.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
-$("#no-button").click(function(){
-	$(this).html("Yes");
+$(".yes-button").each(function(){
 	$(this).click(emailMe);
-});
-
-$("#yes-button").click(emailMe);
+})
 
 function emailMe(){
 	window.location.href = "mailto:kavin.a.subramanyam@gmail.com?subject=What\'s up\?";
